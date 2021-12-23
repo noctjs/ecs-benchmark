00a10b0976703af1ddd2c14bf10bb40f25fe69d1
Florent Cailhol <florent.cailhol@gmail.com>
2021-12-23 11:07:36 +0100

    Format the codebase using prettier (#55)

--- src/cases/piecs/add_remove.js
+++ src/cases/piecs/add_remove.js
@@ -1,31 +1,41 @@
 import { World, createEntitySystem } from "piecs/dist/index.mjs";
 
 export default function createAddRemove(count) {
-    const world = new World();
-    const A = world.createComponentId();
-    const B = world.createComponentId();
+  const world = new World();
+  const A = world.createComponentId();
+  const B = world.createComponentId();
 
-    const prefabA = world.prefabricate([A]);
-    const prefabAB = world.prefabricate([A, B]);
+  const prefabA = world.prefabricate([A]);
+  const prefabAB = world.prefabricate([A, B]);
 
-    world
-        .registerSystem(createEntitySystem(function addB(entities, world) {
-            for (let i = entities.length - 1; i >= 0; i--) {
-                world.transformEntity(entities[i], prefabAB);
-            }
-        }, q => q.every(A)))
-        .registerSystem(createEntitySystem(function removeB(entities, world) {
-            for (let i = entities.length - 1; i >= 0; i--) {
-                world.transformEntity(entities[i], prefabA);
-            }
-        }, q => q.every(B)))
-        .initialize();
+  world
+    .registerSystem(
+      createEntitySystem(
+        function addB(entities, world) {
+          for (let i = entities.length - 1; i >= 0; i--) {
+            world.transformEntity(entities[i], prefabAB);
+          }
+        },
+        (q) => q.every(A)
+      )
+    )
+    .registerSystem(
+      createEntitySystem(
+        function removeB(entities, world) {
+          for (let i = entities.length - 1; i >= 0; i--) {
+            world.transformEntity(entities[i], prefabA);
+          }
+        },
+        (q) => q.every(B)
+      )
+    )
+    .initialize();
 
-    for (let i = 0; i < count; i++) {
-        world.createEntity(prefabA);
-    }
+  for (let i = 0; i < count; i++) {
+    world.createEntity(prefabA);
+  }
 
-    return function addRemove() {
-        world.update();
-    };
-};
+  return function addRemove() {
+    world.update();
+  };
+}
