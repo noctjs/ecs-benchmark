fe6b55c5500956f99d5d3dc6b63863d87bb868d8
Florent Cailhol <florent.cailhol@gmail.com>
2022-01-30 09:09:37 +0100

    Speed up CI

--- src/bench.js
+++ src/bench.js
@@ -3,20 +3,23 @@ import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { Worker } from "node:worker_threads";
 
+const OBJ = "obj";
+const SOA = "soa";
+
 const LIBRARIES = [
-  "becsy",
-  "bitecs",
-  "ecsy",
-  "geotic",
-  "goodluck",
-  "harmony-ecs",
-  "javelin-ecs",
-  "perform-ecs",
-  "picoes",
-  "piecs",
-  "tiny-ecs",
-  "uecs",
-  "wolf-ecs",
+  { kind: SOA, name: "bitecs" },
+  { kind: SOA, name: "harmony-ecs" },
+  { kind: SOA, name: "piecs" },
+  { kind: SOA, name: "wolf-ecs" },
+  { kind: OBJ, name: "becsy" },
+  { kind: OBJ, name: "ecsy" },
+  { kind: OBJ, name: "geotic" },
+  { kind: OBJ, name: "goodluck" },
+  { kind: OBJ, name: "javelin-ecs" },
+  { kind: OBJ, name: "perform-ecs" },
+  { kind: OBJ, name: "picoes" },
+  { kind: OBJ, name: "tiny-ecs" },
+  { kind: OBJ, name: "uecs" },
 ];
 
 const BENCHMARKS = {
@@ -28,16 +31,25 @@ const BENCHMARKS = {
   add_remove: 1_000,
 };
 
-let libraries = [];
+let libraries;
 let args = process.argv.slice(2);
 if (args.length > 0) {
-  for (let lib of args) {
-    if (LIBRARIES.includes(lib)) {
-      libraries.push(lib);
+  let filtered_libraries = [];
+  for (let arg of args) {
+    if (arg[0] === "@") {
+      filtered_libraries.push(
+        ...LIBRARIES.filter((lib) => lib.kind === arg.slice(1))
+      );
     } else {
-      console.warn(`${lib} is not supported`);
+      let lib = LIBRARIES.find((lib) => lib.name === arg);
+      if (lib) {
+        filtered_libraries.push(lib);
+      } else {
+        console.warn(`${lib} is not supported`);
+      }
     }
   }
+  libraries = Array.from(new Set(filtered_libraries));
 } else {
   libraries = LIBRARIES;
 }
@@ -48,10 +60,10 @@ const RESULTS = [];
 for (let lib of libraries) {
   let results = [];
   RESULTS.push(results);
-  console.log(lib);
+  console.log(lib.name);
   for (let name in BENCHMARKS) {
     let log = `  ${name} ${" ".repeat(14 - name.length)}`;
-    let path = resolve(CURRENT_DIR, `./cases/${lib}/${name}.js`);
+    let path = resolve(CURRENT_DIR, `./cases/${lib.name}/${name}.js`);
     if (!existsSync(path)) {
       results.push("TODO");
       console.log(`${log} TODO`);
@@ -81,7 +93,7 @@ console.log("| op/s | " + Object.keys(BENCHMARKS).join(" | ") + " |");
 console.log("| ---- | " + "--: |".repeat(Object.keys(BENCHMARKS).length));
 for (let i = 0; i < libraries.length; i++) {
   console.log(
-    `| ${libraries[i]} | ` +
+    `| ${libraries[i].name} | ` +
       RESULTS[i]
         .map((result) =>
           "hz" in result ? Math.floor(result.hz).toLocaleString() : result
